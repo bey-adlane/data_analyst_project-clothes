591e1449094a2ba8707035a225f18c4bad28afe6
Adlane_BEY <95702979+bey-adlane@users.noreply.github.com>
2025-12-15 17:43:09 +0100

    Add ranking analysis SQL script for products and customers
    This SQL script analyzes product and customer performance by ranking them based on sales revenue and order counts.

---
+++ scripts/06_ranking_analysis.sql
@@ -0,0 +1,78 @@
+/*
+===============================================================================
+Analyse du classement
+===============================================================================
+Objectif :
+- Classer les éléments (produits, clients, etc.) en fonction de leurs performances ou d'autres indicateurs.
+- Identifier les éléments les plus performants et les moins performants.
+
+Fonctions SQL utilisées :
+- Fonctions de classement par fenêtre : RANK(), DENSE_RANK(), ROW_NUMBER(), TOP
+- Clauses : GROUP BY, ORDER BY
+===============================================================================
+*/
+
+-- Quels sont les 5 produits générant le plus de revenus ?
+-- Classement simple
+SELECT TOP 5
+    p.product_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue DESC;
+
+-- Classement complexe mais flexible utilisant 'Window Functions'
+SELECT *
+FROM (
+    SELECT
+        p.product_name,
+        SUM(f.sales_amount) AS total_revenue,
+        RANK() OVER (ORDER BY SUM(f.sales_amount) DESC) AS rank_products
+    FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+        ON p.product_key = f.product_key
+    GROUP BY p.product_name
+) AS ranked_products
+WHERE rank_products <= 5;
+
+-- Quels sont les 5 produits les moins performants en termes de ventes ?
+SELECT TOP 5
+    p.product_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_products p
+    ON p.product_key = f.product_key
+GROUP BY p.product_name
+ORDER BY total_revenue;
+
+-- Trouvez les 10 clients ayant généré les revenus les plus élevés
+SELECT TOP 10
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    SUM(f.sales_amount) AS total_revenue
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_revenue DESC;
+
+-- Les 3 clients ayant passé le moins de commandes
+SELECT TOP 3
+    c.customer_key,
+    c.first_name,
+    c.last_name,
+    COUNT(DISTINCT order_number) AS total_orders
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+    ON c.customer_key = f.customer_key
+GROUP BY 
+    c.customer_key,
+    c.first_name,
+    c.last_name
+ORDER BY total_orders ;
